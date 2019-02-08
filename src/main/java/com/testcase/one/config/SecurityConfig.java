efdf461b3ac0fe5fd49cdc4557ffd6ac5c0d3d54
hariban <beehyv123>
2019-02-08 15:03:16 +0530

    security config added

---
+++ src/main/java/com/testcase/one/config/SecurityConfig.java
@@ -0,0 +1,64 @@
+package com.testcase.one.config;
+import com.testcase.one.Service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+//    @Qualifier("customUserDetailsService")
+            UserService userService;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+
+        return  new BCryptPasswordEncoder();
+    }
+
+    @Autowired
+    public void configureGlobalSecurity(AuthenticationManagerBuilder auth) throws Exception {
+
+        // Setting Service to find User in the database.
+        // And Setting PassswordEncoder
+        auth.userDetailsService(userService).passwordEncoder(passwordEncoder());//passwordEncoder(passwordEncoder());
+//        auth.authenticationProvider(authenticationProvider());
+
+    }
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setUserDetailsService(userService);
+        authenticationProvider.setPasswordEncoder(passwordEncoder());
+        return authenticationProvider;
+    }
+
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.authorizeRequests()
+                .antMatchers("/mycart.html/**").authenticated()
+                .anyRequest().permitAll()
+                .and()//defaultSuccessUrl("/index1.html").permitAll();
+              .formLogin().
+                loginPage("").loginProcessingUrl("/security_login").usernameParameter("username").
+                passwordParameter("password").defaultSuccessUrl("/mycart.html")
+        .and()
+        .logout()
+        .logoutUrl("/logout").logoutSuccessUrl("/index.html");
+
+    }
+
+}
